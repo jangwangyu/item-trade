be45fedb50031907b61389f4ff42944ca04300d3
dkfdj <dkfdj01033@gmail.com>
2025-04-06 04:16:52 +0900

    init

---
+++ src/main/java/org/example/itemtrade/ItemTradeApplication.java
@@ -0,0 +1,13 @@
+package org.example.itemtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemTradeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItemTradeApplication.class, args);
+  }
+
+}
