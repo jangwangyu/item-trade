97e42475c7687cef44f4c32e012d75f4625a235a
dkfdj <dkfdj01033@gmail.com>
2025-04-16 03:13:26 +0900

    #7. memberService와 CustomOAuth2UserService 분리 (From, OAtuth2)

---
+++ src/main/java/org/example/itemtrade/service/CustomOAuth2UserService.java
@@ -0,0 +1,35 @@
+package org.example.itemtrade.service;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.example.itemtrade.domain.Member;
+import org.example.itemtrade.dto.Oauth2.CustomOAuth2User;
+import org.example.itemtrade.repository.MemberRepository;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+  private final MemberRepository memberRepository;
+
+  @Override
+  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    OAuth2User oAuth2User = super.loadUser(userRequest);
+
+    Map<String, Object> kakaoAccount = oAuth2User.getAttribute("kakao_account");// kakao oauth2는 kakao_account에 정보가 담겨있음 그래서 account를 받아와서 정보를 가져와야 함
+    String email = ((String) kakaoAccount.get("email")).trim().toLowerCase();
+    Member member = memberRepository.findByEmail(email).orElseGet(() -> {
+      Member newMember = MemberOauth2.create(userRequest, oAuth2User);
+      return memberRepository.save(newMember);
+    });
+    if (member.isDeleted()) {
+      throw new DisabledException("탈퇴한 회원입니다.");
+    }
+    return new CustomOAuth2User(member, oAuth2User.getAttributes());
+  }
+
+}
--- src/main/java/org/example/itemtrade/service/MemberService.java
+++ src/main/java/org/example/itemtrade/service/MemberService.java
@@ -1,40 +1,17 @@
 package org.example.itemtrade.service;
 
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.example.itemtrade.domain.Member;
-import org.example.itemtrade.dto.Oauth2.CustomOAuth2User;
 import org.example.itemtrade.repository.MemberRepository;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
-public class MemberService extends DefaultOAuth2UserService {
+public class MemberService {
 
   private final MemberRepository memberRepository;
 
-  @Override
-  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-    OAuth2User oAuth2User = super.loadUser(userRequest);
-
-    Map<String, Object> kakaoAccount = oAuth2User.getAttribute("kakao_account");
-    String email = ((String) kakaoAccount.get("email")).trim().toLowerCase();
-    Member member = memberRepository.findByEmail(email).orElseGet(() -> {
-      Member newMember = MemberOauth2.create(userRequest, oAuth2User);
-      return memberRepository.save(newMember);
-    });
-    System.out.println("email=[" + email + "]");
-    if (member.isDeleted()) {
-      throw new DisabledException("탈퇴한 회원입니다.");
-    }
-    return new CustomOAuth2User(member, oAuth2User.getAttributes());
-  }
 
   public void deleteMember(String email) {
     Member member = memberRepository.findByEmail(email)
