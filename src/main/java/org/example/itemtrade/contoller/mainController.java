327d339edab4b2f1b2781c9d3d44826530eafda9
dkfdj <dkfdj01033@gmail.com>
2025-04-18 02:38:37 +0900

    #9. 전체 게시물 목록 보기

--- src/main/java/org/example/itemtrade/contoller/mainController.java
+++ src/main/java/org/example/itemtrade/contoller/mainController.java
@@ -1,18 +1,23 @@
 package org.example.itemtrade.contoller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.itemtrade.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/itemtrade")
+@RequiredArgsConstructor
 @Controller
 public class mainController {
 
-  @GetMapping
-  public String index() {
+  private final PostService postService;
+
+  @GetMapping("/")
+  public String index(Model model) {
+    var posts = postService.getAllPosts();
+    model.addAttribute("posts",posts);
     return "index";
   }
+
 
   @GetMapping("/chat")
   public String chat() {
