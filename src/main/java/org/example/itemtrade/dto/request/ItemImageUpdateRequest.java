dd8b58593f69bcedd31d8604639a621e0710d4bf
dkfdj <dkfdj01033@gmail.com>
2025-04-20 02:35:57 +0900

    #11. 구현 예정 코드

--- src/main/java/org/example/itemtrade/dto/request/ItemImageUpdateRequest.java
+++ src/main/java/org/example/itemtrade/dto/request/ItemImageUpdateRequest.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 
 @Getter
 public class ItemImageUpdateRequest {
-  private List<Long> remainImageIds; // 유지할 이미지 ID들
+  // TODO:  2개 이상의 이미지일 경우 사용(예정)
+  private List<Long> remainImageIds; // 유지할 이미지 ID들(삭제하지 않을 이미지 ID들)
 
   public void setRemainImageIds(List<Long> remainImageIds) {
     this.remainImageIds = remainImageIds;
