48620adf9f97d0c22873c76bae34e7a80ec9910e
dkfdj <dkfdj01033@gmail.com>
2025-04-28 02:19:38 +0900

    #15. 리팩토링

--- src/main/java/org/example/itemtrade/dto/request/ChatMessageRequest.java
+++ src/main/java/org/example/itemtrade/dto/request/ChatMessageRequest.java
@@ -1,28 +1,14 @@
 package org.example.itemtrade.dto.request;
 
-import java.time.LocalDateTime;
-import org.example.itemtrade.domain.ChatMessage;
-import org.example.itemtrade.domain.ChatRoom;
-import org.example.itemtrade.domain.Member;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ChatMessageRequest {
+
     private String content;
 
-    public String getContent() {
-        return content;
-    }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public ChatMessage of(Member sender, ChatRoom chatRoom) {
-        return ChatMessage.builder()
-            .content(this.content)
-            .sender(sender)
-            .chat(chatRoom)
-            .isRead(false)
-            .createdAt(LocalDateTime.now())
-            .build();
-    }
 }
--- src/main/java/org/example/itemtrade/repository/ChatMessageRepository.java
+++ src/main/java/org/example/itemtrade/repository/ChatMessageRepository.java
@@ -4,8 +4,9 @@ import java.util.List;
 import org.example.itemtrade.domain.ChatMessage;
 import org.example.itemtrade.domain.ChatRoom;
 import org.example.itemtrade.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatMessageRepository {
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
   // 채팅방에 속한 메시지를 생성 시간 기준으로 오름차순 정렬해서 가져오기
   List<ChatMessage> findAllByChatRoomOrderByCreatedAtAsc(ChatRoom room);
   // 현재 사용자가 받은 안 읽은 메시지 개수 조회
