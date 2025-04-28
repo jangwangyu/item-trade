c995f5676173ad371e6b47ffbb9686af41532447
dkfdj <dkfdj01033@gmail.com>
2025-04-29 03:13:43 +0900

    #16. 매개변수를 chatRoom으로 교체

--- src/main/java/org/example/itemtrade/repository/ChatMessageRepository.java
+++ src/main/java/org/example/itemtrade/repository/ChatMessageRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
   // 채팅방에 속한 메시지를 생성 시간 기준으로 오름차순 정렬해서 가져오기
-  List<ChatMessage> findAllByChatRoomOrderByCreatedAtAsc(ChatRoom room);
+  List<ChatMessage> findAllByChatRoomOrderByCreatedAtAsc(ChatRoom chatRoom);
   // 현재 사용자가 받은 안 읽은 메시지 개수 조회
   long countByChatRoomAndSenderNotAndIsReadFalse(ChatRoom room, Member currentUser);
 }
