bafbf2027742c0a8b46d24e2a15c7aec8a7fbcf5
dkfdj <dkfdj01033@gmail.com>
2025-05-10 02:24:19 +0900

    #26. 로그인을 한 유저와 안한 유저 처리, 차단한 유저가 있으면 그 유저의 게시글은 안보이게 수정

--- src/main/java/org/example/itemtrade/contoller/mainController.java
+++ src/main/java/org/example/itemtrade/contoller/mainController.java
@@ -2,9 +2,12 @@ package org.example.itemtrade.contoller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.itemtrade.domain.Member;
+import org.example.itemtrade.dto.Oauth2.CustomOAuth2User;
 import org.example.itemtrade.dto.response.ItemPostResponse;
 import org.example.itemtrade.enums.Category;
 import org.example.itemtrade.service.PostService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +20,12 @@ public class mainController {
   private final PostService postService;
 
   @GetMapping("/")
-  public String index(Model model, @RequestParam(required = false) Integer maxPrice, @RequestParam(required = false) Integer minPrice, @RequestParam(required = false) String category) {
+  public String index(@AuthenticationPrincipal CustomOAuth2User user , Model model, @RequestParam(required = false) Integer maxPrice, @RequestParam(required = false) Integer minPrice, @RequestParam(required = false) String category) {
+
+    Member currentUser = (user != null) ? user.getMember() : null;
+
     // 게시글 목록을 가져오는 서비스 메서드 호출
-    List<ItemPostResponse> posts = postService.getAllPosts(category, minPrice, maxPrice);
+    List<ItemPostResponse> posts = postService.getAllPosts(category, minPrice, maxPrice, currentUser);
     model.addAttribute("posts",posts);
     model.addAttribute("categories", Category.values());
     return "index";
