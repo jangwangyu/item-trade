04dc731bb7de414b75f07f2f75525a67e4e0af6a
dkfdj <dkfdj01033@gmail.com>
2025-05-10 02:25:24 +0900

    #26. 유저를 차단하는 메서드와 차단을 해제하는 메서드 추가

--- src/main/java/org/example/itemtrade/service/MemberService.java
+++ src/main/java/org/example/itemtrade/service/MemberService.java
@@ -1,36 +1,63 @@
 package org.example.itemtrade.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.itemtrade.domain.Member;
+import org.example.itemtrade.domain.MemberBlock;
+import org.example.itemtrade.repository.MemberBlockRepository;
 import org.example.itemtrade.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class MemberService {
 
   private final MemberRepository memberRepository;
+  private final MemberBlockRepository memberBlockRepository;
 
+  // 회원 차단
+  public void blockMember(Member blocker, Member blocked) {
+    Member currentUser = getMember(blocker);
+    Member targetUser = getMember(blocked);
 
-  public void deleteMember(String email) {
-    Member member = memberRepository.findByEmail(email)
-        .orElseThrow(() -> new IllegalArgumentException("회원이 아닙니다."));
-    member.delete();
-  }
-
-  @Transactional
-  public void restoreMember(String email) {
-    Member member = memberRepository.findByEmail(email)
-        .orElseThrow(() -> new IllegalArgumentException("회원이 아닙니다."));
-    if (!member.isDeleted()) {
-      throw new IllegalArgumentException("이미 활성화 된 회원입니다.");
+    if(memberBlockRepository.existsByBlockerAndBlocked(currentUser, targetUser)) {
+      throw new IllegalArgumentException("이미 차단된 회원입니다.");
     }
-
-    member.restore();
+    MemberBlock memberBlock = MemberBlock.of(currentUser, targetUser);
+    memberBlockRepository.save(memberBlock);
   }
 
 
 
+  // 회원 차단 해제
+  public void unBlockMember(Member blocker, Member blocked) {
+    Member currentUser = getMember(blocker);
+    Member targetUser = getMember(blocked);
+    MemberBlock memberBlock = memberBlockRepository.findByBlockerAndBlocked(currentUser, targetUser)
+        .orElseThrow(() -> new IllegalArgumentException("차단된 회원이 아닙니다."));
+    memberBlockRepository.delete(memberBlock);
+  }
 
+  public Boolean isMemberBlocked(Member blocker, Member blocked) {
+    return memberBlockRepository.existsByBlockerAndBlocked(blocker, blocked);
+  }
+  
+  // 회원 조회
+  public Member getMember(Member member) {
+    return memberRepository.findById(member.getId()).orElseThrow(() ->
+        new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+  }
+
+  public Member getMemberId(Long memberId) {
+    return memberRepository.findById(memberId).orElseThrow(() ->
+        new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+  }
+
+  public List<Member> getBlockedMembers(Member member) {
+    return memberBlockRepository.findAllByBlocker(member).stream()
+        .map(MemberBlock::getBlocked)
+        .toList();
+  }
 }
