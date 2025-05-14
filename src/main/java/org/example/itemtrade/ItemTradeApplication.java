bfeee6487f0217c909914844802fb1f273e1a0ad
dkfdj <dkfdj01033@gmail.com>
2025-05-15 03:02:56 +0900

    #31. auditing 설정

--- src/main/java/org/example/itemtrade/ItemTradeApplication.java
+++ src/main/java/org/example/itemtrade/ItemTradeApplication.java
@@ -2,8 +2,10 @@ package org.example.itemtrade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ItemTradeApplication {
 
   public static void main(String[] args) {
