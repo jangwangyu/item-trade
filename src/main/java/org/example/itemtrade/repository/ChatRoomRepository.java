8bfeb6c5399176a6dd61f107fdb7521e42be8673
dkfdj <dkfdj01033@gmail.com>
2025-05-15 03:02:29 +0900

    #31. asBuyer와 asSeller의 메서드를 chatRooms로 바꾸고 마지막 메세지 기준으로 정렬하게끔 로직 수정

--- src/main/java/org/example/itemtrade/repository/ChatRoomRepository.java
+++ src/main/java/org/example/itemtrade/repository/ChatRoomRepository.java
@@ -7,6 +7,8 @@ import org.example.itemtrade.domain.ChatRoom;
 import org.example.itemtrade.domain.ItemPost;
 import org.example.itemtrade.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
   // 구매자 채팅방 목록 조회
@@ -22,4 +24,9 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
   // 구매자와 판매자 모두 삭제한 채팅방 목록 조회
   List<ChatRoom> findAllByBuyerAndDeletedByBuyerFalse(Member buyer);
   List<ChatRoom> findAllBySellerAndDeletedBySellerFalse(Member seller);
+
+  // 마지막 메세지로 정렬
+  @Query("SELECT cr FROM ChatRoom cr WHERE (cr.buyer = :member AND cr.deletedByBuyer=false) OR (cr.seller = :member AND cr.deletedBySeller=false) ORDER BY cr.lastMessageCreatedAt DESC")
+  // Chatroom을 cr로 alias하고 cr에 있는 buyer가 member에 있고 deletedByBuyer가 false이거나 seller가 member에 있고 deletedBySeller가 false일 경우 lastMessageCreatedAt으로 정렬
+  List<ChatRoom> findAllByMemberOrderByLastMessage(@Param("member")Member member);
 }
