484a807943ff00b8dc2d2ee8a6372a5045476b67
dkfdj <dkfdj01033@gmail.com>
2025-05-16 03:08:00 +0900

    #32. 상대방 프로필에 거래 완료된 횟수 보기 구현

--- src/main/java/org/example/itemtrade/repository/ItemPostRepository.java
+++ src/main/java/org/example/itemtrade/repository/ItemPostRepository.java
@@ -3,9 +3,18 @@ package org.example.itemtrade.repository;
 import java.util.List;
 import org.example.itemtrade.domain.ItemPost;
 import org.example.itemtrade.domain.Member;
+import org.example.itemtrade.enums.TradeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemPostRepository extends JpaRepository<ItemPost, Long> {
   List<ItemPost> findAllBySeller(Member seller);
   List<ItemPost> findAllByTitleContaining(String keyword); // 검색
+
+  List<ItemPost> findAllBySellerIn(List<Member> blockedMembers);
+
+  // 판매 완료 수
+  int countBySellerAndStatus(Member seller, TradeStatus status);
+
+  // 구매 완료 수
+  int countByBuyerAndStatus(Member buyer, TradeStatus status);
 }
