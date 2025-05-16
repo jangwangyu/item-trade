8ffc78e54504282b8397172d4cff8665896b834a
dkfdj <dkfdj01033@gmail.com>
2025-05-17 03:55:28 +0900

    #33. ofㅏ메서드, imagePath 삭제

--- src/main/java/org/example/itemtrade/dto/request/ItemPostCreateRequest.java
+++ src/main/java/org/example/itemtrade/dto/request/ItemPostCreateRequest.java
@@ -3,8 +3,6 @@ package org.example.itemtrade.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.example.itemtrade.domain.ItemPost;
-import org.example.itemtrade.domain.Member;
 import org.example.itemtrade.enums.Category;
 
 @Getter
@@ -15,25 +13,12 @@ public class ItemPostCreateRequest {
   private String description;
   private int price;
   private Category category;
-  private String imagePath;
 
   public ItemPostCreateRequest(String title, String description, int price, Category category) {
     this.title = title;
     this.description = description;
     this.price = price;
     this.category = category;
-  }
-
-  public ItemPost of(Member seller) {
-    return ItemPost.builder()
-        .title(title)
-        .description(description)
-        .price(price)
-        .category(category)
-        .seller(seller)
-        .imagePath(imagePath)
-        .isSold(false)
-        .build();
   }
 
 }
