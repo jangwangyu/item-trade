457e5a5376c9c2b83fc4b53526212f1b3ab59f69
dkfdj <dkfdj01033@gmail.com>
2025-05-19 04:20:49 +0900

    #35. 일반 form 로그인도 가능하게 구현

--- src/main/java/org/example/itemtrade/config/SecurityConfig.java
+++ src/main/java/org/example/itemtrade/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package org.example.itemtrade.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.itemtrade.dto.User.CustomUserDetails;
 import org.example.itemtrade.service.CustomOAuth2UserService;
+import org.example.itemtrade.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
   private final CustomOAuth2UserService customOAuth2UserService;
+  private final CustomUserDetailsService customUserDetailsService;
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
@@ -20,6 +24,13 @@ public class SecurityConfig {
             .requestMatchers("/", "/login", "/oauth2/**", "/ws/**").permitAll()
             .anyRequest().permitAll()
         )
+        .formLogin(form -> form
+            .loginPage("/login")
+            .defaultSuccessUrl("/") // 로그인 성공 후 이동할 URL
+            .failureUrl("/login?error=true") // 로그인 실패 시 이동할 URL
+            .permitAll()
+        )
+        .userDetailsService(customUserDetailsService)
         .oauth2Login(oauth -> oauth
             .userInfoEndpoint(userInfo -> userInfo
                 .userService(customOAuth2UserService) // 여기서 CustomOAuth2User 생성
