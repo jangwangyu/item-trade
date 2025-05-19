b457434d725dbb238201f82015a03449ad335299
dkfdj <dkfdj01033@gmail.com>
2025-05-20 04:33:28 +0900

    #36. 회원가입 로직 구현

--- src/main/java/org/example/itemtrade/service/MemberService.java
+++ src/main/java/org/example/itemtrade/service/MemberService.java
@@ -6,11 +6,13 @@ import org.example.itemtrade.domain.Member;
 import org.example.itemtrade.domain.MemberBlock;
 import org.example.itemtrade.dto.MemberProfileDto;
 import org.example.itemtrade.dto.User.UserType;
+import org.example.itemtrade.dto.request.MemberJoinRequest;
 import org.example.itemtrade.dto.request.MemberUpdateRequest;
 import org.example.itemtrade.enums.TradeStatus;
 import org.example.itemtrade.repository.ItemPostRepository;
 import org.example.itemtrade.repository.MemberBlockRepository;
 import org.example.itemtrade.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +20,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Service
 public class MemberService {
-
+  private final PasswordEncoder passwordEncoder;
   private final MemberRepository memberRepository;
   private final MemberBlockRepository memberBlockRepository;
   private final ItemPostRepository itemPostRepository;
+
+  // 회원 가입
+  public void join(MemberJoinRequest request) {
+
+    if (memberRepository.existsByEmail(request.getEmail())) {
+      throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+    }
+    if (!request.getPassword().equals(request.getConfirmPassword())) {
+      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+    }
+    if (memberRepository.existsByNickName(request.getNickname())) {
+      throw new IllegalArgumentException("이미 가입된 닉네임입니다.");
+    }
+    if (request.getNickname().length() < 2 || request.getNickname().length() > 10) {
+      throw new IllegalArgumentException("닉네임은 2자 이상 10자 이하로 입력해주세요.");
+    }
+    if (request.getPassword().length() < 8 || request.getPassword().length() > 20) {
+      throw new IllegalArgumentException("비밀번호는 8자 이상 20자 이하로 입력해주세요.");
+    }
+    String encodedPassword = passwordEncoder.encode(request.getPassword());
+    Member memberJoin = request.toEntity(encodedPassword);
+
+    memberRepository.save(memberJoin);
+  }
 
   // 회원 수정
   public void updateMember(Member member, MemberUpdateRequest memberUpdateRequest, UserType userType) {
