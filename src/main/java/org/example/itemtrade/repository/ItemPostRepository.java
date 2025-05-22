7cfdc07cae5e147583c09cf7cf8c5faaf19b9d07
dkfdj <dkfdj01033@gmail.com>
2025-05-22 20:16:35 +0900

    #38. 비즈니스로직에 있던 조건문을 Query로 변경 후 Page 적용

--- src/main/java/org/example/itemtrade/repository/ItemPostRepository.java
+++ src/main/java/org/example/itemtrade/repository/ItemPostRepository.java
@@ -3,10 +3,28 @@ package org.example.itemtrade.repository;
 import java.util.List;
 import org.example.itemtrade.domain.ItemPost;
 import org.example.itemtrade.domain.Member;
+import org.example.itemtrade.enums.Category;
 import org.example.itemtrade.enums.TradeStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemPostRepository extends JpaRepository<ItemPost, Long> {
+  // 전체조회
+  @Query("""
+  SELECT p FROM ItemPost p
+  WHERE (:category IS NULL OR p.category = :category)
+  AND (:minPrice IS NULL OR p.price >= :minPrice)
+  AND (:maxPrice IS NULL OR p.price <= :maxPrice)
+  AND (:blockedMembers IS NULL OR p.seller NOT IN :blockedMembers)
+""")
+  Page<ItemPost> findAllPosts(@Param("category") Category category,
+                                   @Param("minPrice") Integer minPrice,
+                                   @Param("maxPrice") Integer maxPrice,
+                                   @Param("blockedMembers") List<Member> blockedMembers,
+                                   Pageable pageable);
   List<ItemPost> findAllBySeller(Member seller);
   List<ItemPost> findAllByTitleContaining(String keyword); // 검색
 
