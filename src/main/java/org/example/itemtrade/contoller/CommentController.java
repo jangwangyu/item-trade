2753921ada584e8851c61e8d4a3ed450cb3b289b
dkfdj <dkfdj01033@gmail.com>
2025-05-23 03:57:49 +0900

    #39. 댓글 기능을 ajax로 수정했기 때문에 RestController로 수정함

--- src/main/java/org/example/itemtrade/contoller/CommentController.java
+++ src/main/java/org/example/itemtrade/contoller/CommentController.java
@@ -1,35 +1,56 @@
 package org.example.itemtrade.contoller;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
 import lombok.RequiredArgsConstructor;
 import org.example.itemtrade.domain.Member;
+import org.example.itemtrade.dto.CommentDto;
 import org.example.itemtrade.dto.request.CommentCreateRequest;
 import org.example.itemtrade.service.CommentService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/comments")
-@Controller
+@RequestMapping("/api/comments")
+@RestController
 public class CommentController {
 
   private final CommentService commentService;
 
+  @GetMapping("/{postId}")
+  public ResponseEntity<Page<CommentDto>> getComments(@PathVariable Long postId, @PageableDefault(size = 5, sort = "createdAt", direction = DESC) Pageable pageable) {
+
+    Page<CommentDto> comments = commentService.commentList(postId, pageable);
+    return ResponseEntity.ok(comments);
+  }
+
   // 댓글 작성
   @PostMapping("/{postId}")
-  public String addComment(CommentCreateRequest request, @PathVariable Long postId, @AuthenticationPrincipal(expression = "member") Member member) {
-    commentService.addComment(request, postId, member);
-    return "redirect:/posts/" + postId;
+  public ResponseEntity<CommentDto> addComment(
+      @PathVariable Long postId,
+      @AuthenticationPrincipal(expression = "member") Member member,
+      @RequestBody CommentCreateRequest request) {
+
+    CommentDto comment = commentService.addComment(request, postId, member);
+    System.out.println("💬 요청된 댓글 내용: " + request.content());
+    return ResponseEntity.ok(comment);
   }
 
   // 댓글 삭제
-  @DeleteMapping("/{commentId}/delete")
-  public String deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal(expression = "member") Member member) {
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<Void> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal(expression = "member") Member member) {
 
-    Long postId = commentService.deleteComment(commentId, member);
-    return "redirect:/posts/" + postId;
+    commentService.deleteComment(commentId, member);
+    return ResponseEntity.noContent().build();
   }
 }
