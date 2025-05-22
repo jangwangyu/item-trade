c5cedca69aeeb7c841662b3fdd4bba98ae13190e
dkfdj <dkfdj01033@gmail.com>
2025-05-23 03:57:10 +0900

    #39. List를 page로 변경

--- src/main/java/org/example/itemtrade/repository/CommentRepository.java
+++ src/main/java/org/example/itemtrade/repository/CommentRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 import org.example.itemtrade.domain.Comment;
 import org.example.itemtrade.domain.ItemPost;
 import org.example.itemtrade.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
   // 특정 게시글에 달린 모든 댓글을 작성 시간 기준 내림차순으로 조회
-  List<Comment> findAllByItemPostOrderByCreatedAtDesc(ItemPost post);
+  Page<Comment> findAllByItemPostOrderByCreatedAtDesc(ItemPost post, Pageable pageable);
 
   List<Comment> findAllByWriter(Member member);
 }
