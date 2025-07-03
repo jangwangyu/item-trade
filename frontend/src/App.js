22704737a28badb5663af3257899c224d227f627
dkfdj <dkfdj01033@gmail.com>
2025-07-04 00:28:09 +0900

    #65. feature/react-migration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>/App.js</code> and save to reload.
         </p>
         <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
-          <h1 className="text-3xl font-bold text-blue-600">Tailwind CSS 성공못함asasdasdd</h1>
+          <h1 className="text-3xl font-bold text-blue-600">제발 잘 되게 해주세요</h1>
           <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
             버튼 예시
           </button>
