8a7b8ea662679bf8c5f91acada0cdc713da93816
dkfdj <dkfdj01033@gmail.com>
2025-07-06 06:13:59 +0900

    #67. Router path 추가 구현

--- frontend/src/components/AppRouter.js
+++ frontend/src/components/AppRouter.js
@@ -4,13 +4,23 @@ import ChatList from '../pages/ChatList';
 import MyPage from '../pages/MyPage';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import ChatRoom from "../pages/ChatRoom";
+import PostDetail from "../pages/PostDetail";
+import PostEdit from "../pages/PostEdit";
+import PostWrite from "../pages/PostWrite";
+import Profile from "../pages/Profile";
 
 function AppRouter() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ChatRoom" element={<ChatRoom />} />
         <Route path="/chat-list" element={<ChatList />} />
+        <Route path="/PostDetail" element={<PostDetail />} />
+        <Route path="/PostWrite" element={<PostWrite />} />
+        <Route path="/PostEdit" element={<PostEdit />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/Profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
