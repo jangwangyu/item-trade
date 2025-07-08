8d9b1759c1fea91121f2c6008be74674ad5f95af
dkfdj <dkfdj01033@gmail.com>
2025-07-09 03:08:23 +0900

    #67. JoinColumn 추가

--- src/main/java/org/example/itemtrade/domain/Like.java
+++ src/main/java/org/example/itemtrade/domain/Like.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -25,9 +26,11 @@ public class Like {
   private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
   private Member member;
 
   @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
   private ItemPost itemPost;
 
   private LocalDateTime createdAt = LocalDateTime.now();
